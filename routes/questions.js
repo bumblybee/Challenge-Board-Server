389f4b1302cfef8b4dba0aa475bd9b4fb627a193
Tiffani Hess <hesstjune@gmail.com>
2020-07-28 17:55:16 -0500

    removed id param in questions route

--- routes/questions.js
+++ routes/questions.js
@@ -3,6 +3,5 @@ const router = express.Router();
 const questionsController = require("../controllers/questionsController");
 
 router.get("/", questionsController.getQuestions);
-router.get("/:id", questionsController.getQuestions);
 
 module.exports = router;
