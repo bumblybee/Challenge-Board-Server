33749831d12ef2cba8af6631055869f6bdb4346e
Tiffani Hess <hesstjune@gmail.com>
2020-09-03 17:28:05 -0500

    added secure to cookie config if env is prod

--- config.js
+++ config.js
@@ -1,1 +1,11 @@
-exports.COOKIE_CONFIG = { httpOnly: true, maxAge: 3600000 };
+const cookieEnvConfig = {};
+
+if (process.env.NODE_ENV === "development") {
+  //nothing extra
+}
+
+if (process.env.NODE_ENV === "production") {
+  cookieEnvConfig.secure = true;
+}
+
+exports.COOKIE_CONFIG = { httpOnly: true, maxAge: 3600000, ...cookieEnvConfig };
