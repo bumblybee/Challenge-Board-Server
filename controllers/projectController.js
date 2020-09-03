be1b7573757779026326fa4affa4455ac18c7e93
Tiffani Hess <hesstjune@gmail.com>
2020-09-03 18:12:28 -0500

    added custom error handling to editProject

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -2,11 +2,10 @@ const Project = require("../db").Project;
 const { CustomError } = require("../handlers/errorHandlers");
 
 exports.submitProject = async (req, res) => {
-  throw new CustomError("post.failed", "ProjectError", 500);
   const { githubLink, additionalLink, comment } = req.body;
   const { id: userId } = req.token.data;
   const project = { githubLink, additionalLink, comment, userId };
-
+  //TODO: validate links are url
   const newProject = await Project.create(project);
 
   if (newProject) {
@@ -18,25 +17,22 @@ exports.submitProject = async (req, res) => {
 };
 
 exports.editProject = async (req, res) => {
-  //TODO: validate links are url
-  try {
-    const { id: id } = req.token.data;
-    const { githubLink, additionalLink, comment, userId } = req.body;
-    if (userId === id) {
-      const project = await Project.update(
-        {
-          githubLink: githubLink,
-          additionalLink: additionalLink,
-          comment: comment,
-        },
-        { where: { id: req.params.id } }
-      );
+  const { id: id } = req.token.data;
+  const { githubLink, additionalLink, comment, userId } = req.body;
+  if (userId === id) {
+    const project = await Project.update(
+      {
+        githubLink: githubLink,
+        additionalLink: additionalLink,
+        comment: comment,
+      },
+      { where: { id: req.params.id } }
+    );
 
-      if (project) {
-        res.status(200).json(project);
-      }
+    if (project) {
+      res.status(200).json(project);
+    } else {
+      throw new CustomError("post.failed", "ProjectError", 500);
     }
-  } catch (err) {
-    console.log(err);
   }
 };
