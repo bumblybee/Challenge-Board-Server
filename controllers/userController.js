518e696fee5220442878b343c323d7baa4f3eb36
Tiffani Hess <hesstjune@gmail.com>
2020-09-03 18:36:03 -0500

    added custom error handling to passwordReset

--- controllers/userController.js
+++ controllers/userController.js
@@ -117,89 +117,77 @@ exports.getPosts = async (req, res) => {
 
 // set up password reset token and send email with url
 exports.generatePasswordReset = async (req, res) => {
-  try {
-    //Grab email from client
-    const { email } = req.body;
+  //Grab email from client
+  const { email } = req.body;
 
-    //Find email in db
-    const userRecord = User.findOne({ where: { email: email } });
+  //Find email in db
+  const userRecord = User.findOne({ where: { email: email } });
 
-    //If no email match, don't tell them that - pass message that email will be sent
-    if (!userRecord) {
-      res.json({ message: "An email has been sent to the address provided." });
+  //If no email match, don't tell them that - pass message that email will be sent
+  if (!userRecord) {
+    res.json({ message: "An email has been sent to the address provided." });
 
-      // else if email match
-    } else {
-      // create reset password token
-      const resetToken = crypto.randomBytes(25).toString("hex");
-      // set expiration of token
-      const resetExpiry = Date.now() + 1000 * 60 * 60;
-      // update db with reset password token and expiry
-      await User.update(
-        { resetPasswordToken: resetToken, resetPasswordExpiry: resetExpiry },
-        { where: { email } }
-      );
+    // else if email match
+  } else {
+    // create reset password token
+    const resetToken = crypto.randomBytes(25).toString("hex");
+    // set expiration of token
+    const resetExpiry = Date.now() + 1000 * 60 * 60;
+    // update db with reset password token and expiry
+    await User.update(
+      { resetPasswordToken: resetToken, resetPasswordExpiry: resetExpiry },
+      { where: { email } }
+    );
 
-      // create link with current host and reset token - req.headers.host is host and port number of server req is sent to
-      const resetPasswordUrl = `http://localhost:3000/reset-password/${resetToken}`;
+    // create link with current host and reset token - req.headers.host is host and port number of server req is sent to
+    const resetPasswordUrl = `http://localhost:3000/reset-password/${resetToken}`;
 
-      // send email containing link and pass url to ejs template
-      emailHandler.sendEmail({
-        subject: "Password Reset for Challenge Board",
-        filename: "resetPasswordEmail",
-        user: { email },
+    // send email containing link and pass url to ejs template
+    emailHandler.sendEmail({
+      subject: "Password Reset for Challenge Board",
+      filename: "resetPasswordEmail",
+      user: { email },
 
-        resetPasswordUrl,
-      });
+      resetPasswordUrl,
+    });
 
-      //send json confirmation
-      res.json({ message: "An email has been sent to the address provided." });
-    }
-  } catch (err) {
-    console.log(err);
+    //send json confirmation
+    res.json({ message: "An email has been sent to the address provided." });
   }
 };
 
 // When user clicks URL in email, handle password reset and store in db
 exports.passwordReset = async (req, res) => {
-  try {
-    // Grab token from url params
-    const { token } = req.params;
-    // Find user in db where token matches reset token and password expiry is greater than right now
-    const userRecord = await User.findOne({
-      where: {
-        resetPasswordToken: token,
-        resetPasswordExpiry: { [Op.gt]: Date.now() },
-      },
+  // Grab token from url params
+  const { token } = req.params;
+  // Find user in db where token matches reset token and password expiry is greater than right now
+  const userRecord = await User.findOne({
+    where: {
+      resetPasswordToken: token,
+      resetPasswordExpiry: { [Op.gt]: Date.now() },
+    },
+  });
+  // If no matching user with reset token in db, or if token expired
+  if (!userRecord) {
+    throw new CustomError("auth.noToken", "PasswordResetError", 401);
+    // else if matching user
+  } else {
+    // Grab new password from form
+    const { password } = req.body;
+    // await hashed password
+    const hash = await argon2.hash(password);
+    // Update user in db
+    userRecord.update({ password: hash });
+    // send along another cookie with token so they're logged in
+
+    res.cookie("jwt", authService.generateJWT(userRecord), COOKIE_CONFIG);
+    // Send data
+    res.json({
+      message: "Password Updated",
+
+      id: userRecord.id,
+      email: userRecord.email,
+      username: userRecord.username,
     });
-    // If no matching user with reset token in db, or if token expired
-    if (!userRecord) {
-      res.json({
-        message:
-          "Token not found or has expired. Try resetting your password again.",
-      });
-
-      // else if matching user
-    } else {
-      // Grab new password from form
-      const { password } = req.body;
-      // await hashed password
-      const hash = await argon2.hash(password);
-      // Update user in db
-      userRecord.update({ password: hash });
-      // send along another cookie with token so they're logged in
-      //TODO: add to authService
-      res.cookie("jwt", authService.generateJWT(userRecord), COOKIE_CONFIG);
-      // Send data
-      res.json({
-        message: "Password Updated",
-
-        id: userRecord.id,
-        email: userRecord.email,
-        username: userRecord.username,
-      });
-    }
-  } catch (err) {
-    console.log(err);
   }
 };
