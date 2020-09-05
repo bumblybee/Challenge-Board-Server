7094fe14afae98cb89a979c1131b4c00bb8364fb
Tiffani Hess <hesstjune@gmail.com>
2020-09-05 16:56:32 -0500

    removed uri from discord oauth2 constructors for login and signup, in discordService, so that can have only one oauth constructor and use for both login and signup, added uri prop to tokenResponses and generateDiscordUrls

--- services/discordService.js
+++ services/discordService.js
@@ -4,32 +4,26 @@ const authService = require("./authService");
 const { CustomError } = require("../handlers/errorHandlers");
 
 // set up the service with some base information
-const signupOauth = new DiscordOauth2({
+const oauth = new DiscordOauth2({
   clientId: process.env.DISCORD_CLIENT, // provided when you sign up on Discord for an app
-  clientSecret: process.env.DISCORD_SECRET, // provided when you sign up on Discord for an app
-  redirectUri: "http://localhost:3000/discord-signup", // add this as redirect URI in Discord app config
+  clientSecret: process.env.DISCORD_SECRET, // can redirectUri  here so you don't repeat yourself, but need two different uris for login and signup
 });
 
-const loginOauth = new DiscordOauth2({
-  clientId: process.env.DISCORD_CLIENT, // provided when you sign up on Discord for an app
-  clientSecret: process.env.DISCORD_SECRET, // provided when you sign up on Discord for an app
-  redirectUri: "http://localhost:3000/discord-login", // add this as redirect URI in Discord app config
-});
-
-// wraps DiscordOauth2's generate auth url.
 exports.generateSignupDiscordURL = () => {
-  const url = signupOauth.generateAuthUrl({
+  const url = oauth.generateAuthUrl({
     scope: ["identify", "email"], // see Discord OAuth docs for scope info
-    state: crypto.randomBytes(16).toString("hex"), // look familiar? cryptographically-secure random 16 bytes
+    state: crypto.randomBytes(16).toString("hex"),
+    redirectUri: "http://localhost:3000/discord-signup",
   });
 
   return url;
 };
 
 exports.generateLoginDiscordURL = () => {
-  const url = loginOauth.generateAuthUrl({
+  const url = oauth.generateAuthUrl({
     scope: ["identify", "email"], // see Discord OAuth docs for scope info
     state: crypto.randomBytes(16).toString("hex"), // look familiar? cryptographically-secure random 16 bytes
+    redirectUri: "http://localhost:3000/discord-login",
   });
 
   return url;
@@ -38,16 +32,17 @@ exports.generateLoginDiscordURL = () => {
 exports.createDiscordUser = async (code) => {
   // grab an access_token from Discord based on the code and any prior scope
 
-  const tokenResponse = await signupOauth.tokenRequest({
+  const tokenResponse = await oauth.tokenRequest({
     code: code,
     scope: "identify email",
     grantType: "authorization_code", // check the Discord OAuth docs for various grantTypes
+    redirectUri: "http://localhost:3000/discord-signup",
   });
 
   const { access_token } = tokenResponse;
 
   // now that we have the access_token, let's get some user information
-  const discordUser = await signupOauth.getUser(access_token);
+  const discordUser = await oauth.getUser(access_token);
 
   if (discordUser) {
     const { email, username } = discordUser;
@@ -68,15 +63,16 @@ exports.createDiscordUser = async (code) => {
 };
 
 exports.loginDiscordUser = async (code) => {
-  const tokenResponse = await loginOauth.tokenRequest({
+  const tokenResponse = await oauth.tokenRequest({
     code: code,
     scope: "identify email",
     grantType: "authorization_code",
+    redirectUri: "http://localhost:3000/discord-login",
   });
 
   const { access_token } = tokenResponse;
 
-  const discordUser = await loginOauth.getUser(access_token);
+  const discordUser = await oauth.getUser(access_token);
 
   if (discordUser) {
     const { email, username } = discordUser;
