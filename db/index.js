91e115f0222d17963999769376592bbdf4b478f8
Tiffani Hess <hesstjune@gmail.com>
2021-03-15 16:33:27 -0500

    added dialect options to sequelize db connection - require ssl true - to get around sequelize errors

--- db/index.js
+++ db/index.js
@@ -18,7 +18,15 @@ if (env !== "production") {
     { dialect: "postgres", logging: false }
   );
 } else {
-  sequelize = new Sequelize(process.env.DATABASE_URL);
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  });
 }
 
 const User = UserModel(sequelize, Sequelize);
